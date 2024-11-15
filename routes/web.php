8de4c0d309dbfd4c81d88a65d1f626dbe389fa97
Руслан <kindrachukruslan@gmail.com>
2024-11-15 13:48:45 +0000

    magazin

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use App\Http\Controllers\Category\CreateController;
+use App\Http\Controllers\Category\DeleteController;
+use App\Http\Controllers\Category\EditController;
+use App\Http\Controllers\Category\ShowController;
+use App\Http\Controllers\Category\StoreController;
+use App\Http\Controllers\Category\UpdateController;
+use App\Http\Controllers\Main\IndexController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/', IndexController::class)->name('main.index');
+
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', IndexController::class)->name('category.index');
+    Route::get('/create', CreateController::class)->name('category.create');
+    Route::post('/', StoreController::class)->name('category.store');
+    Route::get('/{category}/edit', EditController::class)->name('category.edit');
+    Route::get('/{category}',ShowController::class)->name('category.show');
+    Route::patch('/{category}',UpdateController::class)->name('category.update');
+    Route::delete('/{category}',DeleteController::class)->name('category.delete');
+});
