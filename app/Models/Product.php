d225e8bb8ea4e318d7e67cbb6006dd28914241d2
Руслан <kindrachukruslan@gmail.com>
2024-11-30 13:01:10 +0000

    w

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,5 +12,19 @@ class Product extends Model
     // Разрешаем массовое заполнение для поля 'title'
     protected $fillable =
         ['title', 'description', 'content', 'price', 'count', 'category_id', 'preview_image'];
+    //після цього ті дані які є тут внеслися в базу даних таблицю Products
+
+    public function category() //звязок між  моделями Category і Products
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function getImageUrlAttribute(){    //шлях до картинки апсолютний
+
+      return url('storage/'. $this->preview_image);
+
+    }
+
+
 }
-//після цього ті дані які є тут внеслися в базу даних таблицю Products
+
